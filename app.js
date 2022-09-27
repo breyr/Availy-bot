f82294f53c6430e0aed4956556f83966a4f66ef9
breyr <breyr2021@gmail.com>
2022-09-26 22:36:01 -0400

    refactor of request off command

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const workspaceAuth = require('./database/auth/store_user_workspace_install');
 const db = require('./database/db');
 const dbQuery = require('./database/find_user');
 const nodemailer = require('nodemailer');
+const {
+  buildBodyParserMiddleware,
+} = require('@slack/bolt/dist/receivers/ExpressReceiver');
 require('dotenv').config();
 
 const AVAILY_POSTS_CHANNEL = 'availy-posts';
@@ -112,116 +115,129 @@ const app = new App({
 // change to setemail
 let user_name = 'user';
 app.command('/requestoff', async ({ ack, payload, context }) => {
-  await ack();
+  // acknowledge request
+  ack();
 
   // current date format
   const d = new Date().toLocaleDateString().split('/');
 
-  console.log(payload);
-  console.log(context);
+  console.log('Payload: \n' + payload);
+  console.log('Context: \n' + context);
 
-  user_name = body.user_name;
-  if (body.channel_name == 'directmessage') {
-    console.log(body.channel_name);
-    await say({
-      blocks: [
-        {
-          type: 'header',
-          text: {
-            type: 'plain_text',
-            text: 'Please Select Your Request',
-            emoji: true,
-          },
-        },
-        {
-          type: 'input',
-          element: {
-            type: 'datepicker',
-            initial_date: `${d[2]}-${d[0]}-${d[1]}`,
-            placeholder: {
+  user_name = payload.user_name;
+  if (payload.channel_name == 'directmessage') {
+    try {
+      const result = await app.client.chat.postMessage({
+        token: context.botToken,
+        // Channel to send message to
+        channel: payload.channel_id,
+        // Include a button in the message (or whatever blocks you want!)
+        blocks: [
+          {
+            type: 'header',
+            text: {
               type: 'plain_text',
-              text: 'Select a date',
+              text: 'Please Select Your Request',
               emoji: true,
             },
-            action_id: 'datepicker-action',
           },
-          label: {
-            type: 'plain_text',
-            text: 'Shift Date',
-            emoji: true,
-          },
-        },
-        {
-          type: 'input',
-          element: {
-            type: 'timepicker',
-            initial_time: '00:00',
-            placeholder: {
-              type: 'plain_text',
-              text: 'Select time',
-              emoji: true,
-            },
-            action_id: 'time-start-action',
-          },
-          label: {
-            type: 'plain_text',
-            text: 'Start Shift Time',
-            emoji: true,
-          },
-        },
-        {
-          type: 'input',
-          element: {
-            type: 'timepicker',
-            initial_time: '00:00',
-            placeholder: {
-              type: 'plain_text',
-              text: 'Select time',
-              emoji: true,
-            },
-            action_id: 'time-end-action',
-          },
-          label: {
-            type: 'plain_text',
-            text: 'End Shift Time',
-            emoji: true,
-          },
-        },
-        {
-          type: 'actions',
-          elements: [
-            {
-              type: 'button',
-              text: {
+          {
+            type: 'input',
+            element: {
+              type: 'datepicker',
+              initial_date: `${d[2]}-${d[0]}-${d[1]}`,
+              placeholder: {
                 type: 'plain_text',
+                text: 'Select a date',
                 emoji: true,
-                text: 'Confirm',
               },
-              style: 'primary',
-              action_id: 'confirm_click',
+              action_id: 'datepicker-action',
             },
-            {
-              type: 'button',
-              text: {
+            label: {
+              type: 'plain_text',
+              text: 'Shift Date',
+              emoji: true,
+            },
+          },
+          {
+            type: 'input',
+            element: {
+              type: 'timepicker',
+              initial_time: '00:00',
+              placeholder: {
                 type: 'plain_text',
+                text: 'Select time',
                 emoji: true,
-                text: 'Cancel',
               },
-              style: 'danger',
-              action_id: 'cancel_click',
+              action_id: 'time-start-action',
             },
-          ],
-        },
-      ],
-      text: 'Please select your request for time off.',
-    });
+            label: {
+              type: 'plain_text',
+              text: 'Start Shift Time',
+              emoji: true,
+            },
+          },
+          {
+            type: 'input',
+            element: {
+              type: 'timepicker',
+              initial_time: '00:00',
+              placeholder: {
+                type: 'plain_text',
+                text: 'Select time',
+                emoji: true,
+              },
+              action_id: 'time-end-action',
+            },
+            label: {
+              type: 'plain_text',
+              text: 'End Shift Time',
+              emoji: true,
+            },
+          },
+          {
+            type: 'actions',
+            elements: [
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  emoji: true,
+                  text: 'Confirm',
+                },
+                style: 'primary',
+                action_id: 'confirm_click',
+              },
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  emoji: true,
+                  text: 'Cancel',
+                },
+                style: 'danger',
+                action_id: 'cancel_click',
+              },
+            ],
+          },
+        ],
+        text: 'Please select your request for time off.',
+      });
+    } catch (error) {
+      console.log(error);
+    }
   } else {
     // post a message only visible to user who called the command
-    app.client.chat.postEphemeral({
-      channel: body.channel_id,
-      user: body.user_id,
-      text: '*/requestoff* can only be called in your direct message with Availy',
-    });
+    try {
+      const result = await app.client.chat.postEphemeral({
+        channel: payload.channel_id,
+        user: payload.user_id,
+        text: '*/requestoff* can only be called in your direct message with Availy',
+      });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
   }
 });
 
@@ -274,75 +290,116 @@ app.action('datepicker-action', async ({ body }) => {
   date = split_date[1] + '/' + split_date[2] + '/' + split_date[0];
 });
 
-app.action('confirm_click', async ({ body, ack, say }) => {
+app.action('confirm_click', async ({ ack, body, context }) => {
   // needs to take the date, start and end time from the form and post it as another form in a different channel
-  const messageID = body.message.ts;
-  const channel = body.channel.id;
 
-  await ack(
-    app.client.chat.delete({
-      token: process.env.SLACK_BOT_TOKEN,
-      channel: channel,
-      ts: messageID,
-    })
-  );
+  ack();
 
-  await say(
-    `_*request sent* at ${new Date().toLocaleTimeString('en-US', {
-      timeZone: 'America/New_York',
-    })}_`
-  );
-
-  app.client.chat.postMessage({
-    token: process.env.SLACK_BOT_TOKEN,
-    channel: AVAILY_POSTS_CHANNEL, // this sucks because it is hardcoded, but has to be that way
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `<@${user_name}> is requesting off on \n *Date*: ${date} \n *Time*: ${startTime} - ${endTime}`,
-        },
-      },
-      {
-        type: 'actions',
-        elements: [
-          {
-            type: 'button',
-            text: {
-              type: 'plain_text',
-              text: 'Cover Shift',
-              emoji: true,
-            },
-            style: 'primary',
-            value: 'click_me_123',
-            action_id: 'cover_shift_click',
+  try {
+    // update message
+    const result = await app.client.chat.update({
+      token: context.botToken,
+      // ts of message to update
+      ts: body.message.ts,
+      channel: body.channel.id,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `_*request sent* at ${new Date().toLocaleTimeString('en-US', {
+              timeZone: 'America/New_York',
+            })}_`,
           },
-        ],
-      },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `_clicking cover shift will delete this message and email ITSS_`,
         },
-      },
-    ],
-    text: 'new shift needed coverage posted',
-  });
+      ],
+    });
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // await ack(
+  //   app.client.chat.delete({
+  //     token: process.env.SLACK_BOT_TOKEN,
+  //     channel: channel,
+  //     ts: messageID,
+  //   })
+  // );
+
+  // await say(
+  //   `_*request sent* at ${new Date().toLocaleTimeString('en-US', {
+  //     timeZone: 'America/New_York',
+  //   })}_`
+  // );
+
+  try {
+    const result = await app.client.chat.postMessage({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: AVAILY_POSTS_CHANNEL, // this sucks because it is hardcoded, but has to be that way
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `<@${user_name}> is requesting off on \n *Date*: ${date} \n *Time*: ${startTime} - ${endTime}`,
+          },
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Cover Shift',
+                emoji: true,
+              },
+              style: 'primary',
+              value: 'click_me_123',
+              action_id: 'cover_shift_click',
+            },
+          ],
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `_clicking cover shift will delete this message and email ITSS_`,
+          },
+        },
+      ],
+      text: 'new shift needed coverage posted',
+    });
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.action('cancel_click', async ({ body, ack }) => {
-  const messageID = body.message.ts;
-  const channel = body.channel.id;
+app.action('cancel_click', async ({ ack, body, context }) => {
+  // const messageID = body.message.ts;
+  // const channel = body.channel.id;
+  // await ack(
+  //   app.client.chat.delete({
+  //     token: process.env.SLACK_BOT_TOKEN,
+  //     channel: channel,
+  //     ts: messageID,
+  //   })
+  // );
 
-  await ack(
-    app.client.chat.delete({
-      token: process.env.SLACK_BOT_TOKEN,
-      channel: channel,
-      ts: messageID,
-    })
-  );
+  ack();
+
+  try {
+    const result = await channel.chat.delete({
+      token: context.botToken,
+      channel: body.channel.id,
+      ts: body.message.ts,
+    });
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.action('cover_shift_click', async ({ body, ack, say }) => {
