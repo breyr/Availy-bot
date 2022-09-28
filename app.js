61316af33741b636fb82984c0b4540009cc399fd
breyr <breyr2021@gmail.com>
2022-09-28 18:55:25 -0400

    testing email service

--- app.js
+++ app.js
@@ -496,6 +496,8 @@ app.action('cover_shift_click', async ({ ack, body, context }) => {
   // get the correct shift
   // check to see if the person covering is not the person in the shiftlet userName;
 
+  console.log(`Cover shift body: ${body.message}`);
+
   await ack();
 
   let userName;
